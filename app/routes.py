8b17baf33594b24c7692fa8c0e38c8a05c2fb59c
wilsonvetdev <wilsongch92@gmail.com>
2021-04-12 19:48:41 -0400

    data able to commit and render on home/index page

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,30 @@
 from app.forms import LoginForm, AddStudentForm, AddQuizForm
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Student, Quiz, Result
 
 @app.route("/index")
 @login_required
 def index():
 
-    return render_template("index.html", title="Home")
+    students = Student.query.all()
+    quizzes = Quiz.query.all()
+
+    return render_template("index.html", title="Home", students=students, quizzes=quizzes)
 
 
 @app.route("/student/add", methods=['GET', 'POST'])
 def add_student():
 
     add_student_form = AddStudentForm()
+
+    if add_student_form.validate_on_submit():
+        student = Student(first_name=add_student_form.first_name.data, last_name=add_student_form.last_name.data)
+        db.session.add(student)
+        db.session.commit()
+        flash("You successfully added a new student!")
+        return redirect(url_for("index"))
 
     return render_template("add_student.html", title="Add Student", add_student_form=add_student_form)
 
@@ -23,6 +33,13 @@ def add_student():
 def add_quiz():
 
     add_quiz_form = AddQuizForm()
+
+    if add_quiz_form.validate_on_submit():
+        quiz = Quiz(subject=add_quiz_form.subject.data, num_of_questions=add_quiz_form.num_of_questions.data, date=add_quiz_form.date.data)
+        db.session.add(quiz)
+        db.session.commit()
+        flash("You successfully added a new quiz!")
+        return redirect(url_for("index"))
 
     return render_template("add_quiz.html", title="Add Quiz", add_quiz_form=add_quiz_form)
 
