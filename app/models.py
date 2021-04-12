ae1ad41f475f92b95e21ea35d7994385fe725383
wilsonvetdev <wilsongch92@gmail.com>
2021-04-11 20:40:36 -0400

    tables

--- app/models.py
+++ app/models.py
@@ -7,3 +7,29 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}> - instance of class User.' 
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return f'<Student {self.first_name} {self.last_name} - instance of class Student>'
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(32), index=True)
+    num_of_questions = db.Column(db.Integer)
+    date = db.Column(db.String(64))
+    
+    def __repr__(self):
+        return f'<Quiz instance - subject:{self.subject}, num_of_questions:{self.num_of_questions}, date:{self.date}>'
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"))
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Result instance>'
